af5027e3b42786f4fa01390cac29afb45a8c765b
2point718281 <k.mirok@surreyschools.ca>
2024-11-11 15:42:16 -0800

    Update env.py

--- env.py
+++ env.py
@@ -1,3 +1,6 @@
-from dotenv import load_dotenv
+import os
+def load_dotenv(filename):
+  with open(filename) as f:
+    os.environ.update(dict([tuple(i.split('=')) for i in f.readlines()]))
 
 load_dotenv("resourcecenter.env")
